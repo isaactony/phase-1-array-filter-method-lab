2bf0ad6f88eae1d41a4e8c005d1b91f28d630ffe
isaac <isaactonyloi@gmail.com>
2022-11-29 12:29:29 +0300

    filter function in js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(arr, query) {
+    return arr.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+  }
+
+
+function fuzzyMatch(arr, query) {
+    return arr.filter((el) => el.substring(0,2).toLowerCase().includes(query.substring(0,2).toLowerCase()));
+  }
+
+function matchName(collection, value) {
+    return collection.filter((driver) => {return driver.name === value});
+   
+}
